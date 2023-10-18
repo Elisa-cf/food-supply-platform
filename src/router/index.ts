63dc492f65b2256f06ab4b259feb187cc1aa8210
Elisa <elisacanyelles@gmail.com>
2023-10-18 17:25:15 +0200

    check which routes requires authentication

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import signIn from '../views/SignIn.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,26 @@ const router = createRouter({
       component: () => import('../views/SignIn.vue'),
     },
   ],
+});
+
+//check which routes/pages requires authentication
+router.beforeEach(async (to, from, next) => {
+  if (to.meta.requiresAuth) {
+    try {
+      // Check if the user is authenticated
+      const authenticated = await new signIn();
+      if (authenticated) {
+        next(); // Proceed to the requested route
+      } else {
+        next('/signin'); // Redirect to the signin page if not authenticated
+      }
+    } catch (error) {
+      console.error('Authentication check failed:', error);
+      next('/signin'); // Redirect to the signin page on error
+    }
+  } else {
+    next(); // Proceed to the requested route (when no authentication required)
+  }
 });
 
 export default router;
