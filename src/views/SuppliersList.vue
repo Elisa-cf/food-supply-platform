34de15ade3874a4c7fb8f40625f490900fbe8561
Elisa <elisacanyelles@gmail.com>
2023-10-19 17:09:51 +0200

    splitting code in supplierList more reusable code creating a child component and adding the loading spinner code into the utils folder

--- src/views/SuppliersList.vue
+++ src/views/SuppliersList.vue
@@ -1,45 +1,20 @@
 <template>
   <div>
-    <!-- Display when isLoading is true -->
-    <div v-if="isLoading" class="flex justify-center mt-10">
-      <div class="lds-roller">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
+    <LoadingSpinner :isLoading="isLoading" />
 
-    <!-- Display the list of suppliers when not loading and no error -->
     <div
-      v-else-if="!isLoading"
+      v-if="!isLoading"
       class="flex flex-col gap-3 w-11/12 mx-auto max-w-4xl justify-center my-3 text-purple2"
     >
       <ul
         class="grid grid-cols-1 gap-2 xs:grid-cols-2 xs:gap-2 xs:gap-x-4 lg:gap-x-8 items-center"
       >
-        <li
+        <SuppliersListItem
           v-for="supplier in suppliers"
           :key="supplier.id"
-          class="bg-blue1 mt-2 px-2 py-4 rounded-md gap-2 sm:px-4 lg:flex lg:p-8 lg:gap-3"
-        >
-          <p class="justify-self-start">
-            <strong>Name:</strong> {{ supplier.name }}
-          </p>
-          <p class="justify-self-start">
-            <strong>Description:</strong> {{ supplier.description }}
-          </p>
-          <button
-            @click="() => viewSupplierDetail(supplier.id)"
-            class="mt-2 bg-yellow1 text-purple1 font-bold py-1 px-4 rounded-full text-sm lg:flex-end lg:px-5 lg:py-3"
-          >
-            View Details
-          </button>
-        </li>
+          :supplier="supplier"
+          :viewSupplierDetail="viewSupplierDetail"
+        />
       </ul>
       <button @click="loadMoreSuppliers" v-if="hasMore">Load More</button>
     </div>
@@ -50,6 +25,8 @@
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { getAuthToken, fetchSuppliers } from '../utils/api';
+import SuppliersListItem from '../components/SuppliersListItem.vue';
+import LoadingSpinner from '../utils/LoadingSpinner.vue';
 
 interface Supplier {
   id: string;
@@ -103,5 +80,3 @@ onMounted(async () => {
   await loadMoreSuppliers();
 });
 </script>
-
-<style scoped></style>
